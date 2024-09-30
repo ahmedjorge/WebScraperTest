42541a21ab2b87461dccd3cbc86e241799de00c3
ahmedjorge <ahmedferreira10@hotmail.com>
2024-09-30 19:37:14 +0000

    adcionado o link no resultado

--- scraper.ts
+++ scraper.ts
@@ -6,8 +6,14 @@ type Laptop = {
     price: number;
     description: string;
     imageUrl: string,
-    link: string
+    link: string;
+    prices: HD[]
 };
+
+type HD = {
+    volume: number,
+    price?: number
+}
 
 function prepareString (str: string): string {
     return str.replace(/\s+/g, '').toLowerCase();
@@ -15,10 +21,10 @@ function prepareString (str: string): string {
 
 export async function Laptops(brand: string): Promise<Laptop[]> {
     let page = 1;
-    let hasNextPage = true;
     let allLaptops: Laptop[] = [];
-    let site = `https://webscraper.io`;
+    let hasNextPage = true;
 
+    let site = `https://webscraper.io`;
     brand = prepareString(brand);
 
     while (hasNextPage) {
@@ -26,29 +32,58 @@ export async function Laptops(brand: string): Promise<Laptop[]> {
         let { data } = await axios.get(url);
         let $ = cheerio.load(data);
 
-        let laptopsOnPage: Laptop[] = $('.card.thumbnail').toArray().map((element) => {
+        $('.card.thumbnail').each( async (_, element) => {
             let title = $(element).find('.caption h4 a').attr("title") || "";
-            let price = $(element).find('.caption .price').text().replace('$', '');
+            let price = +($(element).find('.caption .price').text().replace('$', '') || 0);
             let link = $(element).find('.caption a[href]').attr('href') || '';
             let description = $(element).find('.caption p').text();
             let imageUrl = $(element).find('img').attr('src') || '';
 
-            return {
+            let linkProd = `${site}${link}`;
+            let { data } = await axios.get(linkProd);
+            let $$ = cheerio.load(data);
+            let hds : HD[] = [];
+
+            $$(".swatches").find(".swatch").map((_, element)=> {
+                if(!$$(element).is(":disabled")){
+                    let volume = +($$(element).attr("value") || 0);
+                    let priceByHD = price;
+
+                    if(volume === 256){
+                        priceByHD += 20;
+                    }else if(volume === 512){
+                        priceByHD += 40;
+                    }else if(volume === 1024){
+                        priceByHD += 60;
+                    }
+
+                    hds.push({
+                        volume: +volume,
+                        price: priceByHD
+                    })
+                }
+            });
+
+            console.log(hds);
+
+            allLaptops.push( {
                 title,
-                price: parseFloat(price),
+                price: +price,
                 description,
                 imageUrl: `${site}${imageUrl}`,
-                link: `${site}${link}`
-            };
-        }).filter(({title, description}) => prepareString(title).includes(brand) || prepareString(description).includes(brand));
-
-        allLaptops = allLaptops.concat(laptopsOnPage);
+                link: linkProd,
+                prices: hds
+            });
+        });
 
         let nextPageButton = $('.pagination li').last();
-        hasNextPage = !nextPageButton.hasClass('disabled');
 
+        hasNextPage = !nextPageButton.hasClass('disabled');
         page++;
+
     }
 
-    return allLaptops.sort((a, b) => a.price - b.price);
+    let laptops = allLaptops.filter(({title, description}) => prepareString(title).includes(brand) || prepareString(description).includes(brand))
+
+    return laptops.sort((a, b) => a.price - b.price);
 }
